5a8934c1bf2a34dd99eabf5bab411f3938060a5d
Scottie Cornett <scottie.cornett@gmail.com>
2022-11-12 20:17:11 -0500

    logic added to the MortgageReport class

--- MortgageCalculator/MortgageReport.cs
+++ MortgageCalculator/MortgageReport.cs
@@ -8,5 +8,35 @@ namespace MortgageCalculator
 {
     public class MortgageReport
     {
+        private static MortgageCalculator? calculator;
+
+        public static void PrintMortgage(int principal, float annualInterest, byte years)
+        {
+            calculator = new MortgageCalculator(principal, annualInterest, years);
+            double mortgage = calculator.CalculateMortgage();
+            string mortgageFormatted = mortgage.ToString("C");
+            Console.WriteLine();
+            Console.WriteLine("MORTGAGE");
+            Console.WriteLine("--------");
+            Console.WriteLine("MORTGAGE PAYMENT: " + mortgageFormatted);
+        }
+
+        public static void PrintPaymentSchedule()
+        {
+
+            Console.WriteLine();
+            Console.WriteLine("PAYMENT SCHEDULE");
+            Console.WriteLine("----------------");
+
+            for (short month = 1; month <= calculator.GetNumberOfPayments(); month++)
+            {
+
+                double balance = calculator.CalculateBalance(month);
+                var displayBalance = balance.ToString("C");
+                Console.WriteLine(displayBalance + " " + "Payment number: " + month);
+            }
+
+        }
+
     }
 }
