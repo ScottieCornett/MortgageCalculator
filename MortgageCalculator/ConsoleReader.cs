d67f2eb8779f753970afd91cf84f697b9085c83e
Scottie Cornett <scottie.cornett@gmail.com>
2022-11-12 20:13:49 -0500

    logic added to ConsoleReader

--- MortgageCalculator/ConsoleReader.cs
+++ MortgageCalculator/ConsoleReader.cs
@@ -8,5 +8,32 @@ namespace MortgageCalculator
 {
     public class ConsoleReader
     {
+        public static double ReadNumber(string prompt, double min, double max)
+        {
+            double value;
+
+            while (true)
+            {
+
+                Console.Write(prompt);
+                var input = Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(input))
+                {
+                    Console.WriteLine("Please enter a valid value");
+                    continue;
+                }
+
+                bool parsed = double.TryParse(input, out value);
+
+                if (parsed && value > min && value <= max)
+                    break;
+
+                Console.WriteLine("Please enter a number between " + min + " and " + max);
+
+            }
+            return value;
+        }
+
+
     }
 }
